ce0682bb5cb0e420e936e8a22083aec670f5bd2e
Organ94 <99993473+Organ94@users.noreply.github.com>
2023-04-12 22:38:11 +0300

    Adding model classes:  - User.java  - StorageFile.java
    Adding request and response classes:
     - request:
      - AuthenticationRQ.java
      - EditFileNameRQ.java
     - response
      - AuthenticationRS.java
      - FileRS.java
      - ExceptionRS.java

---
+++ src/main/java/com/example/diplomawork/model/User.java
@@ -0,0 +1,75 @@
+package com.example.diplomawork.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+@Builder
+public class User implements UserDetails, Serializable {
+
+    @Id
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<StorageFile> userFiles;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
